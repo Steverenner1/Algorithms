48627d4725ef38d3d0a4fd93e6e4979137b6941e
Steve Renner <che1111@hotmail.com>
2020-02-19 13:30:17 -0800

    completed rps.py

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,8 +2,25 @@
 
 import sys
 
+gamePlay = [["rock"], ["paper"], ["scissors"]]
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return [[]]
+  if n == 1: # how many valid plays are done with rock
+    return gamePlay
+ 
+  result = []
+
+  arrA = rock_paper_scissors(n - 1)
+  
+  for subArr in arrA:
+    for play in gamePlay:
+      newPlay = subArr + play
+      result.append(newPlay)
+
+  return result
+
+print(rock_paper_scissors(4))
 
 
 if __name__ == "__main__":
